c08b0abf63e5ea4ac7054c47e502c80d467e24ae
raymondngiam <ha.ngiam87@gmail.com>
2020-09-27 16:21:36 +0800

    Add custom pipe to convert - to space

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent
+    AppComponent, 
+    ProductListComponent, 
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule, FormsModule
